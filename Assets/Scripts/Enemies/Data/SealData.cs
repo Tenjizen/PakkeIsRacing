4c3ccd0affdd8fd217c67898039f9d03d535b311
MatteoBenaissa <matteobenaissa@gmail.com>
2023-05-02 19:26:26 +0200

    [SCRIPT] - Created seal controller & data scripts

---
+++ Assets/Scripts/Enemies/Data/SealData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Enemies.Data
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SealData", order = 1)]
+    public class SealData : ScriptableObject
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enemies/Seal/SealController.cs
@@ -0,0 +1,13 @@
+using GPEs;
+using UnityEngine;
+
+namespace Enemies.Seal
+{
+    public class SealController : MonoBehaviour
+    {
+        [Header("Player detection"), SerializeField] private PlayerTriggerManager _frontPlayerTrigger;
+        [SerializeField] private PlayerTriggerManager _backPlayerTrigger;
+    
+        
+    }
+}
