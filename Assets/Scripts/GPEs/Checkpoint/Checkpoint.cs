0c96c9f183d2e295840d068a0b88de2a7abfd7d8
LEPERS_NICOLAS\nicol <nicolaslepers176@gmail.com>
2023-05-20 23:06:41 +0200

    [SCRIPT] - checkpoint add next checkpoint for sedna

--- Assets/Scripts/GPEs/Checkpoint/Checkpoint.cs
+++ Assets/Scripts/GPEs/Checkpoint/Checkpoint.cs
@@ -14,6 +14,7 @@ namespace GPEs.Checkpoint
         [Header("References"), SerializeField] private ZoneManager _zoneManager;
         [SerializeField] private Transform _targetRespawnTransform;
         [SerializeField] private ParticleSystem _activationParticles;
+        public Checkpoint NextCheckPoint;
 
         private StudioEventEmitter _emitter;
 
--- Assets/Scripts/GPEs/Checkpoint/CheckpointManager.cs
+++ Assets/Scripts/GPEs/Checkpoint/CheckpointManager.cs
@@ -6,15 +6,21 @@ namespace GPEs.Checkpoint
 {
     public class CheckpointManager : MonoBehaviour
     {
-        [field:SerializeField] public Checkpoint CurrentCheckpoint { get; private set; }
+        [field: SerializeField] public Checkpoint CurrentCheckpoint { get; private set; }
 
         private Transform _baseTransform;
-
+        public bool SednaIsMoving = false;
+        float TimerSednaIsMoving;
         private void Start()
         {
             _baseTransform = CharacterManager.Instance.transform;
         }
 
+        private void Update()
+        {
+            if (SednaIsMoving == true)
+                PlayerRespawn();
+        }
         public Transform GetRespawnPoint()
         {
             if (CurrentCheckpoint == null)
@@ -29,5 +35,28 @@ namespace GPEs.Checkpoint
         {
             CurrentCheckpoint = checkpoint;
         }
+        
+        public void PlayerRespawn()
+        {
+            if (CurrentCheckpoint.NextCheckPoint == null)
+            {
+                return;
+            }
+            CharacterManager.Instance.SednaGameObject.SetActive(true);
+            TimerSednaIsMoving += Time.deltaTime;
+            if (TimerSednaIsMoving <= 3)
+            {
+                CharacterManager.Instance.SednaGameObject.transform.LookAt(CurrentCheckpoint.NextCheckPoint.transform);
+                CharacterManager.Instance.SednaGameObject.transform.Translate(Vector3.forward * (5 * Time.deltaTime), Space.Self);
+                //move sedna
+            }
+            else
+            {
+                SednaIsMoving = false;
+                CharacterManager.Instance.SednaGameObject.SetActive(false);
+            }
+
+        }
+
     }
 }
