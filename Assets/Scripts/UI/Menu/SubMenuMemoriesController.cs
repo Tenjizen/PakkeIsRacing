ae3158b7bcfe1bc8171e2d7f764eea9aab08aa9c
MatteoBenaissa <matteobenaissa@gmail.com>
2023-06-09 20:47:39 +0200

    [SCRIPT] - memories menu

--- Assets/Scripts/UI/Menu/SubMenuMemoriesController.cs
+++ Assets/Scripts/UI/Menu/SubMenuMemoriesController.cs
@@ -23,16 +23,10 @@ namespace UI.Menu
         [Header("Sub Menu"), SerializeField] private List<MenuUIObject> _objectsList = new List<MenuUIObject>();
         [SerializeField] private TMP_Text _dialogText;
         [SerializeField] private MemoryUIObject _memoryUIObjectPrefab;
-        [SerializeField] private Transform _dialogsUIObjectLayout;
+        [SerializeField] private Transform _memoriesUIObjectLayout;
         [SerializeField] private List<MemoryCategory> _memoryCategories = new List<MemoryCategory>();
 
         private int _index;
-
-        protected override void Start()
-        {
-            base.Start();
-            JsonFilesManagerSingleton.Instance.CollectibleJsonFileManagerProperty.OnNewCollectibleGet.AddListener(SetTilesData);
-        }
 
         public override void SetMenu(bool isActive, bool isUsable)
         {
@@ -96,33 +90,19 @@ namespace UI.Menu
             _dialogText.text = memoryUIObject == null ? _objectsList[_index].GetName() : memoryUIObject.MemoryText;
         }
 
-        private void SetTilesData()
-        {
-            for (int i = 0; i < _objectsList.Count; i++)
-            {
-                MemoryUIObject memoryUIObject = _objectsList[i].GetComponent<MemoryUIObject>();
-                
-                if (memoryUIObject == null ||
-                    i >= JsonFilesManagerSingleton.Instance.MemoriesJsonFileManagerProperty.CollectedDialogs.Count || 
-                    JsonFilesManagerSingleton.Instance.MemoriesJsonFileManagerProperty.CollectedDialogs[i] == null)
-                {
-                    continue;
-                }
-
-                DialogCreator dialog = JsonFilesManagerSingleton.Instance.MemoriesJsonFileManagerProperty.CollectedDialogs[i].DialogCreatorGameObject;
-                memoryUIObject.AddDialogData(CharacterManager.Instance.Parameters.Language ? dialog.Dialog_EN : dialog.Dialog_FR);
-            }
-        }
-        
         private void CreateDialogsUIObject()
         {
-            int count = JsonFilesManagerSingleton.Instance.MemoriesJsonFileManagerProperty.CollectedDialogs.Count;
-            Height = count;
-            
-            for (int i = 0; i < _memoryCategories.Count; i++) //loop se relance trop de fois et a chaque fois pas de verif si la category est la bonnne
+            foreach (Transform child in _memoriesUIObjectLayout.transform)
             {
-                bool isCategoryCreated = false;
-                MemoryUIObject currentObject = new MemoryUIObject();
+                Destroy(child.gameObject);
+            }
+
+            _objectsList.Clear();
+            Height = 0;
+            
+            for (int i = 0; i < _memoryCategories.Count; i++)
+            {
+                MemoryUIObject currentObject = null;
                 
                 List<CollectedDialogsData> list = JsonFilesManagerSingleton.Instance.MemoriesJsonFileManagerProperty.CollectedDialogs;
                 for (int j = 0; j < list.Count; j++)
@@ -132,26 +112,27 @@ namespace UI.Menu
                         collectedDialogData.DialogCreatorGameObject.Dialog_EN : 
                         collectedDialogData.DialogCreatorGameObject.Dialog_FR;
 
-                    if (collectedDialogData.IsCollected == false || data == null)
+                    if (collectedDialogData.IsCollected == false || data == null ||
+                        _memoryCategories[i].CategoryID != collectedDialogData.CategoryID) 
                     {
                         continue;
                     }
                  
                     //create object
-                    if (isCategoryCreated == false)
+                    if (currentObject == null)
                     {
-                        isCategoryCreated = true;
-                        currentObject = Instantiate(_memoryUIObjectPrefab, _dialogsUIObjectLayout);
+                        currentObject = Instantiate(_memoryUIObjectPrefab, _memoriesUIObjectLayout);
                         MemoryCategory category = _memoryCategories.Find(x => x.CategoryID == collectedDialogData.DialogCreatorGameObject.CategoryID);
                         currentObject.SetCategoryName(category);
+                        _objectsList.Add(currentObject);
+                        Height++;
                     }
                     
-                    currentObject.AddDialogData(data);
+                    DialogCreator dialog = JsonFilesManagerSingleton.Instance.MemoriesJsonFileManagerProperty.CollectedDialogs[j].DialogCreatorGameObject;
+                    currentObject.AddDialogData(CharacterManager.Instance.Parameters.Language ? dialog.Dialog_EN : dialog.Dialog_FR);
                 }
-
             }
-
-            SetTilesData();
+            
             SetTile();
         }
     }
